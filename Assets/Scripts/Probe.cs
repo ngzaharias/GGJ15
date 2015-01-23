7a945eb7305a00e83a6b315066bec4401a5ff6ee
NyanPrime <Joakimay@gmail.com>
2015-01-23 10:53:16 +0100

    Commented SetWaterLevel

--- Assets/Scripts/Probe.cs
+++ Assets/Scripts/Probe.cs
@@ -5,6 +5,8 @@ public class Probe : MonoBehaviour
 {
 	Vector3 _waterLevelAtProbe;
 
+	/// <summary>Sets the height of the debugger probe target</summary>
+	/// <param name="waterLevel"></param>
 	public void SetWaterLevel(float waterLevel)
 	{
 		_waterLevelAtProbe = new Vector3(transform.position.x, waterLevel, transform.position.z);
