987767e0367a958901dd01b6c41065d135705b69
NyanPrime <Joakimay@gmail.com>
2015-01-23 11:02:46 +0100

    Stripped down and cleaned up

--- Assets/Scripts/EffectsProbe.cs
+++ Assets/Scripts/EffectsProbe.cs
@@ -5,49 +5,33 @@ public class EffectsProbe : MonoBehaviour
 {
 	[SerializeField] GameObject _foamPrefab;
 
-	int 			_simulationCycle = 0;
-	float 			_previousLevel;
-	ParticleSystem 	_particleSystem;
+	float _previousLevel;
 
 	void Start()
 	{
 		_previousLevel = transform.position.y;
-		_particleSystem = GetComponent<ParticleSystem>();
-
-		_particleSystem.enableEmission = false;
 	}
 
 	void FixedUpdate()
 	{
-//		return;
-		Vector3 position = transform.position;
-
-		float probeWaterLevel = BuoyancyManager.Instance.GetHeight(transform.position);
-		float deltaLevel = position.y - probeWaterLevel;
+		float currentLevel = transform.position.y;
+		float waterLevelAtProbe = BuoyancyManager.Instance.GetWaterLevelAtPosition(transform.position);
+		float levelDifference = currentLevel - waterLevelAtProbe;
 		
-		bool enteredWater = _previousLevel > probeWaterLevel && position.y < probeWaterLevel;
-		bool exitedWater = _previousLevel < probeWaterLevel && position.y > probeWaterLevel;
+		bool enteredWater = _previousLevel > waterLevelAtProbe && currentLevel < waterLevelAtProbe;
+		bool exitedWater = _previousLevel < waterLevelAtProbe && currentLevel > waterLevelAtProbe;
 		
 		if (enteredWater || exitedWater)
 		{
-			float power = Mathf.Abs(deltaLevel);
-//			power = Mathf.Clamp(power, 0.0f, 0.05f);
-			_particleSystem.startSpeed = (power * Random.Range(500, 1000)) + 1.0f;
-			_particleSystem.Emit((int)(power * 1000) + 1);
-			
-//			GameObject particle = (GameObject)Instantiate(_foamPrefab, transform.position, Quaternion.identity);
-//			float force = Random.Range(-10, 10);
-//			particle.rigidbody.AddForce(new Vector3(force, force, force));
-		}
-//		else if (Mathf.Abs(deltaLevel) < 0.05f)
-		else if (deltaLevel > 0 && deltaLevel < 0.1f)
-		{
-			float power = Mathf.Abs(deltaLevel);
-			_particleSystem.startSpeed = 0;
-			_particleSystem.Emit(Random.Range(0, 4) == 0 ? 1 : 0);
+			// Probe entered or exited the water
 		}
 
-		_previousLevel = position.y;
+		if (Mathf.Abs(levelDifference) < 0.1f)
+		{
+			// Probe is close to the water level
+		}
+
+		_previousLevel = currentLevel;
 	}
 
 	void OnDrawGizmos()
@@ -55,7 +39,7 @@ public class EffectsProbe : MonoBehaviour
 		if (BuoyancyManager.PROBE_VISUALIZE)
 		{
 			Gizmos.color = Color.blue;
-			Gizmos.DrawSphere(transform.position, BuoyancyManager.PROBE_SIZE * 0.25f);
+			Gizmos.DrawSphere(transform.position, BuoyancyManager.PROBE_SIZE * 0.5f);
 		}
 	}
 }
