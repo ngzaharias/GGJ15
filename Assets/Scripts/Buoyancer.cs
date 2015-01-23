b79bcf8b50a159326b1d08c4c94ecec202b3b225
NyanPrime <Joakimay@gmail.com>
2015-01-23 10:55:06 +0100

    Refactoring

--- Assets/Scripts/Buoyancer.cs
+++ Assets/Scripts/Buoyancer.cs
@@ -5,30 +5,30 @@ public class Buoyancer : MonoBehaviour
 {
 	[SerializeField] float 			_buoyancy = 100;
 
-	Probe[] _probes;
+	Probe[] _buoyancyProbes;
 
 	float 	_ProbeContribution;
 
 	void Start ()
 	{
-		_probes = GetComponentsInChildren<Probe>();
-		_ProbeContribution = 1.0f / (float)_probes.Length;
+		_buoyancyProbes = GetComponentsInChildren<Probe>();
+		_ProbeContribution = 1.0f / (float)_buoyancyProbes.Length;
 	}
 	
 	void FixedUpdate ()
 	{
-		for (int i = 0; i < _probes.Length; i++)
+		for (int i = 0; i < _buoyancyProbes.Length; i++)
 		{
-			float waterLevelAtProbe = BuoyancyManager.Instance.GetWaterLevelAtPosition(_probes[i].transform.position);
-			float deltaLevel = waterLevelAtProbe - _probes[i].transform.position.y;
+			Vector3 probePosition = _buoyancyProbes[i].transform.position;
+			float waterLevelAtProbe = BuoyancyManager.Instance.GetWaterLevelAtPosition(probePosition);
+			float deltaLevel = waterLevelAtProbe - probePosition.y;
 			float force = deltaLevel * Time.deltaTime * _buoyancy * _ProbeContribution;
-			Vector3 probePosition = _probes[i].transform.position;
 
 			rigidbody.AddForceAtPosition(new Vector3(0, Mathf.Clamp(force, -5.0f, 9.8f), 0), probePosition);
 
 			if (BuoyancyManager.PROBE_VISUALIZE)
 			{
-				_probes[i].SetWaterLevel(waterLevelAtProbe);
+				_buoyancyProbes[i].SetWaterLevel(waterLevelAtProbe);
 			}
 		}
 	}
