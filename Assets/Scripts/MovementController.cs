6cd9a1fa4b7ce688a5b1de46d284ea42ac34b452
tim-stasse <tim.stasse@gmail.com>
2015-01-24 00:07:39 +1100

    Added steering

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField]
+    SteerPoint _leftSteerPoint;
+    [SerializeField]
+    SteerPoint _rightSteerPoint;
+
+    [SerializeField]
+    float _force = 10;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForceAtPosition(_leftSteerPoint.Direction * _force, _leftSteerPoint.transform.position);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rigidbody.AddForceAtPosition(_rightSteerPoint.Direction * _force, _rightSteerPoint.transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/SteerPoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteerPoint : MonoBehaviour
+{
+    public static bool GIZMO_VISUALIZE = true;
+    public static float GIZMO_SIZE = 0.1f;
+
+    [SerializeField]
+    Transform _steerDirection;
+
+    public Vector3 Direction
+    {
+        get
+        {
+            if (_steerDirection != null)
+            {
+                return (_steerDirection.position - transform.position).normalized;
+            }
+
+            return Vector3.zero;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (GIZMO_VISUALIZE)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, GIZMO_SIZE);
+
+            if (_steerDirection != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(_steerDirection.transform.position, GIZMO_SIZE);
+            }
+        }
+    }
+}
