442ddc1d9b2caa0882ef5d697ac542ce44e1be12
Nicholas Zaharias <applzor@gmail.com>
2015-01-23 23:23:09 +1000

    Added to Camera script

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,21 +3,45 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public GameObject _objectToFollow = null;
+    public Transform _objectToFollow = null;
     public AnimationCurve _followCurve = AnimationCurve.Linear(0, 1, 10, 10);
+
+    public float _zoomSpeed = 100.0f;
+    public float _zoomDistance = 5.0f;
+
+    private Transform _camera = null;
+    private float _scrollDelta = 0.0f;
+    private const float _scrollDeltaFalloff = 0.2f;
+
+    void Awake()
+    {
+        _camera = GetComponentInChildren<Camera>().transform;
+    }
 
     void FixedUpdate()
     {
-        FollowTarget();
+        _scrollDelta -= _scrollDelta * _scrollDeltaFalloff;
+        _scrollDelta += Input.GetAxis("Mouse ScrollWheel");
+
+        TargetFollow();
+        TargetZoom(_scrollDelta);
     }
 
-    void FollowTarget()
+    void TargetFollow()
     {
         Vector3 currentPos = transform.position;
-        Vector3 targetPos = _objectToFollow.transform.position;
-        float distance = Vector3.Magnitude(targetPos - currentPos);
+        Vector3 targetPos = _objectToFollow.position;
+        float distance = Vector3.Distance(currentPos, targetPos);
 
         distance = _followCurve.Evaluate(distance);
         transform.position = Vector3.MoveTowards(currentPos, targetPos, distance * Time.fixedDeltaTime);
+    }
+
+    void TargetZoom(float delta)
+    {
+        float zoomDelta = delta * _zoomSpeed;
+        Vector3 targetPos = _camera.transform.localPosition + (_camera.forward * _zoomDistance);
+
+         _camera.position = Vector3.MoveTowards(_camera.position, targetPos, zoomDelta * Time.fixedDeltaTime);
     }
 }
