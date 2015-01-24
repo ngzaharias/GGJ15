1b2dbcec8be4e3ab830bd753c8042b1972b0c3ed
NyanPrime <Joakimay@gmail.com>
2015-01-24 12:47:38 +0100

    Cthulu lookat

---
+++ Assets/Scripts/Cthulu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cthulu : MonoBehaviour
+{
+	[SerializeField] Transform _target;
+	[SerializeField] Vector3 _lookOffset;
+
+	Transform _neck;
+
+	void Start()
+	{
+		_neck = transform.Find("Root/spine1/neck");
+	}
+
+	void Update()
+	{
+		_neck.transform.LookAt(_target.position, Vector3.right);
+		_neck.transform.eulerAngles -= _lookOffset;
+	}
+}
