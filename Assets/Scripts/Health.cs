9c0275392e6b911d801c0716fb62cbbca5e6344a
NyanPrime <Joakimay@gmail.com>
2015-01-25 05:24:00 +0100

    2 particle systems for both damage and death

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -10,8 +10,12 @@ public class Health : MonoBehaviour
 	[SerializeField] Vector3			_onDeathScale;
 	[SerializeField] ParticleSystem		_particlesToEmitDamage;
 	[SerializeField] int				_onDamageEmission;
+	[SerializeField] ParticleSystem		_particlesToEmitDamage2;
+	[SerializeField] int				_onDamageEmission2;
 	[SerializeField] ParticleSystem		_particlesToEmitDeath;
 	[SerializeField] int				_onDeathEmission;
+	[SerializeField] ParticleSystem		_particlesToEmitDeath2;
+	[SerializeField] int				_onDeathEmission2;
 
 	GameObject _healthBar;
 
@@ -20,6 +24,14 @@ public class Health : MonoBehaviour
 		_healthBar = (GameObject)Instantiate(Resources.Load("Interface/HealthBar"), transform.position + _healthBarOffset, Quaternion.identity);
 		_healthBar.transform.parent = InterfaceManager.Instance.transform.Find("CanvasWorld");
 		_healthBar.GetComponent<Slider>().maxValue = _health;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			Damage(10);
+		}
 	}
 
 	void LateUpdate()
@@ -38,15 +50,19 @@ public class Health : MonoBehaviour
 		if (_health == 0)
 		{
 			GameObject.Destroy(_healthBar);
-			if (_transformToScale)
-				_transformToScale.localScale = _onDeathScale;
 			if (_particlesToEmitDeath)
 				_particlesToEmitDeath.Emit(_onDeathEmission);
+			if (_particlesToEmitDeath2)
+				_particlesToEmitDeath2.Emit(_onDeathEmission2);
+			if (_transformToScale)
+				_transformToScale.localScale = _onDeathScale;
 		}
 		else
 		{
 			if (_particlesToEmitDamage)
 				_particlesToEmitDamage.Emit(_onDamageEmission);
+			if (_particlesToEmitDamage2)
+				_particlesToEmitDamage2.Emit(_onDamageEmission2);
 		}
 	}
 }
