ae91dcbc4d425b889d09f78eb7159f1dc744c6f2
ItsMattyo <matt.peadon@gmail.com>
2015-01-25 15:15:14 +1100

    Updated Credit + main menu

---
+++ Assets/Scripts/Mattshorriblefadescript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+//This is a matt script, pls no kill me
+//Fades an object in/out
+
+var timer : float =10.0;
+
+
+function Start () 
+{
+	renderer.material.color.a =1;
+}
+
+function Update () 
+{
+	timer = timer - Time.deltaTime;
+	if(timer >= 0)
+	{
+		renderer.material.color.a -= 0.1 * Time.deltaTime;
+	}
+	
+	if(timer <=0)
+	{
+		timer = 0;
+		Destroy(gameObject);
+	}
+}
